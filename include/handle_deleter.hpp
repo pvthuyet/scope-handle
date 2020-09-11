46498efd0b0c1a513f8b7bb210dc26638a528145
pvthuyet <pvthuyet@gmail.com>
2020-09-11 15:35:27 +0700

    add more windows handle objects

--- include/handle_deleter.hpp
+++ include/handle_deleter.hpp
@@ -5,25 +5,65 @@ namespace fibo
 {
 	struct CloseHandleDeleter
 	{
-		void operator()(HANDLE hdl) const
+		void operator()(HANDLE hObj) const
 		{
-			::CloseHandle(hdl);
+			::CloseHandle(hObj);
 		}
 	};
 
-	struct CloseFileDeleter
+	struct DeleteObjectDeleter
 	{
-		void operator()(FILE* file) const
+		void operator()(HGDIOBJ hObj) const
 		{
-			::fclose(file);
+			::DeleteObject(hObj);
 		}
 	};
 
 	struct RegCloseKeyDeleter
 	{
-		void operator()(HKEY hdl) const
+		void operator()(HKEY hObj) const
 		{
-			::RegCloseKey(hdl);
+			::RegCloseKey(hObj);
+		}
+	};
+
+	struct CloseFileDeleter
+	{
+		void operator()(FILE* hObj) const
+		{
+			::fclose(hObj);
+		}
+	};
+
+	struct CloseSocketDeleter
+	{
+		void operator()(SOCKET hObj) const
+		{
+			::closesocket(hObj);
+		}
+	};
+
+	struct UnmapViewOfFileDeleter
+	{
+		void operator()(LPVOID hObj) const
+		{
+			::UnmapViewOfFile(hObj);
+		}
+	};
+
+	struct FreeLibraryDeleter
+	{
+		void operator()(HMODULE hObj) const
+		{
+			::FreeLibrary(hObj);
+		}
+	};
+
+	struct FindCloseDeleter
+	{
+		void operator()(HANDLE hObj) const
+		{
+			::FindClose(hObj);
 		}
 	};
 }
